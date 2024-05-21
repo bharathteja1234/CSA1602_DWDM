5cb2e985023c40f5c397fcb4fa1228b7b1f12d4f
Bharath Teja <113326733+bharathteja1234@users.noreply.github.com>
2024-05-21 09:27:02 +0530

    Update and rename 6th problem to Normallization.R

--- Normallization.R
+++ Normallization.R
@@ -1,8 +1,8 @@
 age <- 35
 min_age <- 0
-max_age <- 100  # Assuming the maximum possible age is 100 for simplicity
+max_age <- 100  
 min_max_normalized <- (age - min_age) / (max_age - min_age)
-mean_age <- 0  # Assuming mean age is 0 for simplicity
+mean_age <- 0  
 std_dev_age <- 12.94
 z_score_normalized <- (age - mean_age) / std_dev_age
 scaling_factor <- 10^(ceiling(log10(age)))
